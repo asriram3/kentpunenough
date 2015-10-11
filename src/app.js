725f73f134e120eeac3b2892995cc1828e33669b
asriram3 <asriram3@illinois.edu>
2015-10-11 09:17:37 -0500

    made the api work

--- src/app.js
+++ src/app.js
@@ -1,22 +1,35 @@
-var encodedAuth = 'MmR6ZkpsdlJOdndvRzFkcGdpejBxczl0WTpqb0NQN29Ua2xBdUVzd29jRGwwMTJTTzAxVUxybE1LRVVBMlZvNVZLYjJ1RHRRazc1TQ==';
-//var URL = 'https://api.twitter.com/1.1/search/tweets.json?q=#kenthackenough';
-var URL = 'https://api.twitter.com/oauth2/token';
-//var ajax = require('ajax');
-//var token;
+var bearerAuth = 'Bearer AAAAAAAAAAAAAAAAAAAAAAoriAAAAAAAa3e7UFfTpyPGxQS6r6Jr8ELoOLw%3DNhiK7NbzqXcyiNhx2ftbx3E6jw3jPrawQOxAQwRNa0mA8JTFdZ';
+var URL = 'https://api.twitter.com/1.1/search/tweets.json?q=%23kentpunenough';
+//var URL = 'https://api.twitter.com/oauth2/token';
+var ajax = require('ajax');
+
+var UI = require('ui');
+// Create a Card with title and subtitle
+var card = new UI.Card({
+  title:'Twitter Puns',
+  subtitle:'Fetching...'
+});
+
+// Display the Card
+card.show();
+
+
+
 ajax(
 		{
 			url: URL,
 			type: 'json',
-      method: 'post',
-			headers: { 'Authorization' : encodedAuth,
-      'Content-Type' : 'application/x-www-form-urlencoded;charset=UTF-8',
-                'Content-Length': 29,
-                'Accept-Encoding':'gzip'},
-      data: 'grant_type=client_credentials'
+      method: 'GET',
+			headers: { 'Authorization' : bearerAuth,'Accept-Encoding':'gzip'}
 		},
 		function(data) {
 			//console.log(data);
       console.log('success');
+			console.log(data);
+			
+			
+			
+			
     },function(error) {
       console.log("error");
     });
